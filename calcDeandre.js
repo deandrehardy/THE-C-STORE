7c6ae89264bd994c182776d36e2e3d3d3fb12743
deandrehardy <56272827+deandrehardy@users.noreply.github.com>
2019-10-28 14:57:56 -0400

    EDITS
    :)

--- calcDeandre.js
+++ calcDeandre.js
@@ -1,8 +1,7 @@
-function calcBMI(){
-  w = weight.value  // read weight from element with id="weight"
-  h = height.value  // read height from element with id="height"
-  bmi = 703*w/(h*h)  // calculate the Body Mass Index
-  BMI.innerHTML =    // write into label element with id "BMI"
-      "Your BMI is "
-      +bmi.toFixed(1)  // round BMI to 1 decimal place
+function calcAGE(){
+  a = age.value  // read height from element with id="height"
+  age = 365*a  // calculate the Body Mass Index
+  AGE.innerHTML =    // write into label element with id "BMI"
+      "Your Age in Days is "
+      +age.toFixed(1)  // round BMI to 1 decimal place
 }
