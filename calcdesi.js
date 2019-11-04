f5fa61d4f4f8a24608ea4297cec313f1be77bc70
dtidwell410 <dtidwell@brandeis.edu>
2019-11-04 14:17:55 -0500

    poop
    poops sock

---
+++ calcdesi.js
@@ -0,0 +1,8 @@
+function calcBMI(){
+  c = parseFloat(costs.value)  // read weight from element with id="weight"
+  t = parseFloat(tax.value)  // read height from element with id="height"
+  totalCost = c*t+c  // calculate the Body Mass Index
+  Total.innerHTML =    // write into label element with id "BMI"
+      "it costs <br>"
+      + totalCost.toFixed(2)  // round BMI to 1 decimal place
+}
