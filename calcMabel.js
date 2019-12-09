ce62a89cdde225789fb6a19410ca08cd5c099ea8
messymabel <cinthyamancia@brandeis.edu>
2019-12-09 13:39:40 -0500

    calculator

--- calcMabel.js
+++ calcMabel.js
@@ -1,8 +1,8 @@
-function calcTip(){
-  x = pretax.value  // read weight from element with id="weight"
-  y = taxrate.value  // read height from element with id="height"
-  tip = x*y  // calculate the Body Mass Index
-  Tip.innerHTML =    // write into label element with id "BMI"
-      "Your total tip is "
-      +tip.toFixed(1)  // round BMI to 1 decimal place
+  function calcBMI(){
+  w = weight.value  // read weight from element with id="weight"
+  h = height.value  // read height from element with id="height"
+  bmi = 703*w/(h*h)  // calculate the Body Mass Index
+  BMI.innerHTML =    // write into label element with id "BMI"
+      "Your BMI is "
+      +bmi.toFixed(1)  // round BMI to 1 decimal place
 }
